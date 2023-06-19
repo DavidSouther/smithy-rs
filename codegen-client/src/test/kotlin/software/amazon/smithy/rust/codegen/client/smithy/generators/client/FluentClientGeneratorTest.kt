d4a0b50d7da31e125ecb99ad08b69c3ab735ff37
David Souther <davidsouther+github@gmail.com>
2023-06-19 15:56:26 -0700

    Add get_foo() -> &Option<Foo> accessors for builders and fluent builders.
    This allows testing, as well as making decisions while creating builders.
    The references are not mutable, so these props remain read only keeping the Builder invariants.

--- codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGeneratorTest.kt
+++ codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGeneratorTest.kt
@@ -6,17 +6,26 @@
 package software.amazon.smithy.rust.codegen.client.smithy.generators.client
 
 import org.junit.jupiter.api.Test
+import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.client.testutil.TestCodegenSettings
 import software.amazon.smithy.rust.codegen.client.testutil.clientIntegrationTest
+import software.amazon.smithy.rust.codegen.client.testutil.testSymbolProvider
 import software.amazon.smithy.rust.codegen.core.rustlang.CargoDependency
+import software.amazon.smithy.rust.codegen.core.rustlang.implBlock
 import software.amazon.smithy.rust.codegen.core.rustlang.rust
 import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
 import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
 import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
+import software.amazon.smithy.rust.codegen.core.smithy.generators.BuilderGenerator
+import software.amazon.smithy.rust.codegen.core.smithy.generators.StructureGenerator
+import software.amazon.smithy.rust.codegen.core.testutil.TestWorkspace
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
+import software.amazon.smithy.rust.codegen.core.testutil.compileAndTest
 import software.amazon.smithy.rust.codegen.core.testutil.integrationTest
+import software.amazon.smithy.rust.codegen.core.testutil.unitTest
+import software.amazon.smithy.rust.codegen.core.util.lookup
 
 class FluentClientGeneratorTest {
     val model = """
@@ -31,6 +40,12 @@ class FluentClientGeneratorTest {
 
         operation SayHello { input: TestInput }
         structure TestInput {}
+        
+        @documentation("this documents the shape")
+        structure MyStruct {
+           foo: String,
+           byteValue: Byte,
+        }
     """.asSmithyModel()
 
     @Test
@@ -75,5 +90,34 @@ class FluentClientGeneratorTest {
             TestCodegenSettings.orchestratorModeTestParams,
             test = test,
         )
+    }
+
+    private val struct = model.lookup<StructureShape>("com.example#MyStruct")
+
+    @Test
+    fun `generate inner builders`() {
+        val provider = testSymbolProvider(model)
+        val project = TestWorkspace.testProject(provider)
+        project.moduleFor(struct) {
+            rust("##![allow(deprecated)]")
+            StructureGenerator(model, provider, this, struct, emptyList()).render()
+            implBlock(provider.toSymbol(struct)) {
+                BuilderGenerator.renderConvenienceMethod(this, provider, struct)
+            }
+            unitTest("generate_builders") {
+                rust(
+                    """
+                    let my_struct_builder = MyStructBuilder::builder().byte_value(4).foo("hello!");
+                    assert_eq!(*my_struct_builder.get_foo(), Some("hello!".to_string()));
+                    let inner = my_struct_builder.inner();
+                    assert_eq!(*inner.get_byte_value(), Some(4));
+                    """,
+                )
+            }
+        }
+        project.withModule(provider.moduleForBuilder(struct)) {
+            BuilderGenerator(model, provider, struct, emptyList()).render(this)
+        }
+        project.compileAndTest()
     }
 }
